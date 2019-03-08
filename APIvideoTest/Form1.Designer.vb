a557082775fe409281b23520a4e56da1170ac15b
readdawg <michael.s.reading@gmail.com>
2019-03-08 15:47:07 -0600

    Initial commit

---
+++ APIvideoTest/Form1.Designer.vb
@@ -0,0 +1,165 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.tbUser = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.tbPassword = New System.Windows.Forms.TextBox()
+        Me.tbTimeout = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.tbCamera = New System.Windows.Forms.TextBox()
+        Me.pbVideo = New System.Windows.Forms.PictureBox()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.btnVideo = New System.Windows.Forms.Button()
+        CType(Me.pbVideo, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'tbUser
+        '
+        Me.tbUser.Location = New System.Drawing.Point(71, 10)
+        Me.tbUser.Name = "tbUser"
+        Me.tbUser.Size = New System.Drawing.Size(100, 20)
+        Me.tbUser.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(33, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Login"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 40)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(54, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Paasword"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 67)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(45, 13)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Timeout"
+        '
+        'tbPassword
+        '
+        Me.tbPassword.Location = New System.Drawing.Point(71, 36)
+        Me.tbPassword.Name = "tbPassword"
+        Me.tbPassword.Size = New System.Drawing.Size(100, 20)
+        Me.tbPassword.TabIndex = 4
+        '
+        'tbTimeout
+        '
+        Me.tbTimeout.Location = New System.Drawing.Point(71, 64)
+        Me.tbTimeout.Name = "tbTimeout"
+        Me.tbTimeout.Size = New System.Drawing.Size(100, 20)
+        Me.tbTimeout.TabIndex = 5
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(13, 98)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(43, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Camera"
+        '
+        'tbCamera
+        '
+        Me.tbCamera.Location = New System.Drawing.Point(71, 95)
+        Me.tbCamera.Name = "tbCamera"
+        Me.tbCamera.Size = New System.Drawing.Size(100, 20)
+        Me.tbCamera.TabIndex = 7
+        '
+        'pbVideo
+        '
+        Me.pbVideo.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.pbVideo.Location = New System.Drawing.Point(178, 13)
+        Me.pbVideo.Name = "pbVideo"
+        Me.pbVideo.Size = New System.Drawing.Size(1055, 609)
+        Me.pbVideo.TabIndex = 8
+        Me.pbVideo.TabStop = False
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Location = New System.Drawing.Point(15, 122)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(156, 23)
+        Me.btnSubmit.TabIndex = 9
+        Me.btnSubmit.Text = "Submit"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'btnVideo
+        '
+        Me.btnVideo.Location = New System.Drawing.Point(16, 152)
+        Me.btnVideo.Name = "btnVideo"
+        Me.btnVideo.Size = New System.Drawing.Size(155, 23)
+        Me.btnVideo.TabIndex = 10
+        Me.btnVideo.Text = "Video"
+        Me.btnVideo.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1245, 634)
+        Me.Controls.Add(Me.btnVideo)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Controls.Add(Me.pbVideo)
+        Me.Controls.Add(Me.tbCamera)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.tbTimeout)
+        Me.Controls.Add(Me.tbPassword)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.tbUser)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.pbVideo, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents tbUser As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents tbPassword As TextBox
+    Friend WithEvents tbTimeout As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents tbCamera As TextBox
+    Friend WithEvents pbVideo As PictureBox
+    Friend WithEvents btnSubmit As Button
+    Friend WithEvents btnVideo As Button
+End Class
---
+++ APIvideoTest/Form1.vb
@@ -0,0 +1,56 @@
+﻿Imports TokenAuth
+Imports MjpegProcessor
+Imports System.Drawing
+
+
+Public Class Form1
+
+    Dim encryptedToken As String
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        ' Populate text boxes when form loads
+        tbUser.Text = "administrator"
+        tbPassword.Text = "12345"
+        tbTimeout.Text = "36000"
+        tbCamera.Text = "333228228"
+
+    End Sub
+
+    ' Create encrypted user token
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+
+        Dim user As String = tbUser.Text
+        Dim password As String = tbPassword.Text
+        Dim timeout As Long = tbTimeout.Text
+
+        Dim userToken As New TokenAuth.UserToken(user, password, timeout)
+        encryptedToken = userToken.Encrypt()
+
+        MsgBox(encryptedToken)
+
+    End Sub
+
+    Public Sub New()
+        InitializeComponent()
+    End Sub
+
+    ' Connect to VI API and ingest MJPEG stream
+    Private Sub btnStart_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnVideo.Click
+        Dim mjpeg As New MjpegDecoder
+        Dim ip As String = "10.10.3.33"
+        Dim camera As String = tbCamera.Text
+        Dim url As String = "http://" + ip + ":9000/api/v1/video/" + camera + "/mjpeg?token=" + encryptedToken
+
+        MsgBox("URL =" + url)
+
+        AddHandler mjpeg.FrameReady, AddressOf mjpeg_FrameReady
+        mjpeg.ParseStream(New Uri("http://" + ip + ":9000/api/v1/video/" + camera + "/mjpeg?token=" + encryptedToken))
+    End Sub
+
+    ' Display MJPEG stream in an image box object
+    Private Sub mjpeg_FrameReady(ByVal sender As Object, ByVal e As FrameReadyEventArgs)
+        pbVideo.Image = e.Bitmap
+    End Sub
+
+End Class
